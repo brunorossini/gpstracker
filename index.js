af28321b554c28394e6bc34659d91c531959763c
Bruno Rossini <brunorossini@live.com>
2020-08-24 15:37:51 -0300

    initial commit

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-var gpstracker = require("gpstracker");
+var server = module.exports,
+  net = require("net"),
+  EventEmitter = require("events").EventEmitter;
 
-var server = gpstracker.create().listen(5050, function () {
-  console.log("listening your gps trackers on port", 5050);
-});
-
-server.trackers.on("connected", function (tracker) {
-  console.log("tracker connected with imei:", tracker.imei);
-
-  tracker.on("help me", function () {
-    console.log(tracker.imei + " pressed the help button!!".red);
+server.create = function () {
+  var server = net.createServer(function (client) {
+    var data = "";
+    client.on("data", function (chunk) {
+      data += chunk.toString();
+      if (data.indexOf(";") === -1) return;
+      var messagesToProcess = data.split(";");
+      for (var i = 0; i < messagesToProcess.length - 1; i++) {
+        processData(server, client, messagesToProcess[i]);
+      }
+      data = data.slice(data.lastIndexOf(";") + 1);
+    });
   });
+  server.trackers = new EventEmitter();
+  return server;
+};
 
-  tracker.on("position", function (position) {
-    console.log(
-      "tracker {" + tracker.imei + "}: lat",
-      position.lat,
-      "lng",
-      position.lng
-    );
-  });
+function processData(server, client, data) {
+  console.log(`server: ${server}`);
+  console.log(`client: ${client}`);
+  console.log(`data: ${data}`);
+  console.log(`---------------`);
+}
 
-  tracker.trackEvery(10).seconds();
+server.create().listen(8000, function () {
+  console.log("listening your gps trackers on port", 8000);
 });
